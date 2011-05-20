88a92c2119056ba87ef51c775713cb3dcd9a1533
Russell Haering <russellhaering@gmail.com>
2011-05-20 01:33:47 -0700

    rip out microphone stuff and trigger alert on a timed loop

--- src/com/map/fft/FFTListenerService.java
+++ src/com/map/fft/FFTListenerService.java
@@ -6,10 +6,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.AudioRecord.OnRecordPositionUpdateListener;
-import android.media.MediaRecorder.AudioSource;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
@@ -34,17 +30,11 @@ public class FFTListenerService extends Service {
 	private Notification foregroundNotification;
 	private NotificationManager notificationManager;
 
-	// Recording parameters
-	private int AUDIO_SOURCE = AudioSource.MIC;
-	private int SAMPLE_RATE = 22050;
-	private int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
-	private int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
-	private int CHUNK_SIZE = 4096;
-	private int BUFFER_SIZE = CHUNK_SIZE * 4;
-	private int SIREN_TIMEOUT = 25;
+	// Siren alert timings
+	private final long BEGIN_DELAY = 5 * 1000;
+	private final long END_DELAY = 5 * 1000;
 
 	// The Handler and AudioRecord for recording
-	private AudioRecord micRecord;
 	private Handler micHandler;
 
 	@Override
@@ -138,107 +128,31 @@ public class FFTListenerService extends Service {
 	 */
 	private Runnable micStopper = new Runnable() {
 		public void run() {
-			micRecord.stop();
-			micRecord.release();
-			micRecord = null;
 			Looper.myLooper().quit();
 		}
 	};
 
-	/**
-	 * A thread that gathers and analyzes data from the microphone.
-	 * 
-	 * Because calling read() to gather data from the microphone is a blocking
-	 * operation, we perform it in a background thread so that the main service
-	 * thread doesn't lock.
-	 */
-	private Thread micThread = new Thread() {
-		// Audio recording and analysis state
-		private short audioBuffer[];
-		private FFTSignalDatabase db;
-		private int sirenTimeout = 0;
+	private Runnable startAlert = new Runnable() {
+		public void run() {
+			sendBroadcast(new Intent(SIREN_BEGIN));
+			micHandler.postDelayed(stopAlert, END_DELAY);
+		}
+	};
 
-		/**
-		 * Run the microphone thread.
-		 * 
-		 * The microphone threads job is to gather data from the microphone,
-		 * pass it to the signal database for analysis, then respond to any
-		 * signals (that is, Sirens) reported by the database. The thread
-		 * handles jobs using a Handler that actually belongs to the
-		 * encapsulating FFTListenerService - the advantage of this being that
-		 * the listener service can post a job to the Handler that causes the
-		 * microphone thread to exit cleanly.
-		 * 
-		 * To prepare for all of this we instantiate the signal database and
-		 * handler, then instantiate a new AudioRecord to read data from the
-		 * microphone. We instruct the AudioRecord to trigger a listener after
-		 * each segment of audio is recorded, then kick off the first such
-		 * recording. Finally, we start the thread's Looper which will allow the
-		 * Handler to begin handling jobs.
-		 */
+	private Runnable stopAlert = new Runnable() {
+		public void run() {
+			sendBroadcast(new Intent(SIREN_END));
+			micHandler.postDelayed(startAlert, BEGIN_DELAY);
+		}
+	};
+
+	private Thread micThread = new Thread() {
 		@Override
 		public void run() {
 			Looper.prepare();
-
-			db = new FFTSignalDatabase(SAMPLE_RATE, CHUNK_SIZE);
-
 			micHandler = new Handler();
-
-			Log.i(TAG, "Buffer Size = " + BUFFER_SIZE);
-
-			audioBuffer = new short[BUFFER_SIZE];
-
-			micRecord = new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE,
-					CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE * 2);
-
-			micRecord.setRecordPositionUpdateListener(micBufferListener,
-					micHandler);
-			micRecord.setPositionNotificationPeriod(BUFFER_SIZE);
-			micRecord.startRecording();
-			micRecord.read(audioBuffer, 0, BUFFER_SIZE);
-
+			micHandler.postDelayed(startAlert, BEGIN_DELAY);
 			Looper.loop();
 		}
-
-		/**
-		 * Handle data from the microphone.
-		 * 
-		 * The onPeriodNotification method of this class will be fired each time
-		 * a BUFFER_SIZE sized segment of audio has been recorded. We pass this
-		 * buffer to the signal database, and get back an FFTSignal. If the
-		 * signal is non-null, we broadcast a SIREN_BEGIN then set a counter to
-		 * SIREN_TIMEOUT. Every subsequent period that no signal is detected,
-		 * this counter is decremented, but any time a signal is detected it is
-		 * reset to SIREN_TIMEOUT. When the timer reaches 0 we broadcast a
-		 * SIREN_END.
-		 */
-		private OnRecordPositionUpdateListener micBufferListener = new OnRecordPositionUpdateListener() {
-			// This method is required to exist but we don't use it
-			public void onMarkerReached(AudioRecord recorder) {
-			}
-
-			public void onPeriodicNotification(AudioRecord recorder) {
-				micRecord.read(audioBuffer, 0, BUFFER_SIZE);
-
-				for (int i = 0; i < BUFFER_SIZE; i += CHUNK_SIZE) {
-					int[] vals = FFT.realFFT(audioBuffer, i, CHUNK_SIZE);
-					FFTSignal sig = db.searchChunk(vals);
-
-					if (sig != null) {
-						if (sirenTimeout == 0) {
-							sendBroadcast(new Intent(SIREN_BEGIN));
-						}
-						sirenTimeout = SIREN_TIMEOUT;
-					} else {
-						if (sirenTimeout == 1) {
-							sendBroadcast(new Intent(SIREN_END));
-						}
-						if (sirenTimeout != 0) {
-							sirenTimeout--;
-						}
-					}
-				}
-			}
-		};
 	};
 }
